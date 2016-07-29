604cbdad5e8450aa9e24c90cdcac0f940def215c
Abhijeet Joshi <jabhijeet@gmail.com>
2016-07-29 20:16:56 +0800

    base code
    Change-Id: If0efcd903837d0134bc01769509a3819c353dcaa
    Signed-off-by: Abhijeet Joshi <jabhijeet@gmail.com>

---
+++ src/main/java/org/couchpotato/rentanything/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.couchpotato.rentanything.repository;
+
+import java.util.List;
+
+import org.couchpotato.rentanything.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+	List<User> findByUsername(@Param("username") String username);
+
+}
